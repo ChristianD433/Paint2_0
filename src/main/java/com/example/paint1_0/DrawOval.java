3b0f973e10519de673c2b99098829d1ddda52262
Christian Arias <christiandaniel136@gmail.com>
2023-01-30 21:11:58 +0100

    Se ha reestructurado el proyecto y añadido la capacidad de pintar rectangulos y hexagonos llenos y vacios

--- src/main/java/com/example/paint1_0/DrawOval.java
+++ src/main/java/com/example/paint1_0/DrawOval.java
@@ -3,27 +3,25 @@ package com.example.paint1_0;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.TextField;
-import javafx.scene.paint.Color;
 
-public class DrawRectangle extends DrawShape {
+public class DrawOval extends DrawShape {
+    private boolean esRelleno = false;
     protected Double x2 = 0D;
     protected Double y2 = 0D;
-    boolean esRelleno = false;
 
-    public DrawRectangle(GraphicsContext graphicsContext) {
+    public DrawOval(GraphicsContext graphicsContext) {
         super(graphicsContext);
     }
 
     @Override
     public void draw(ColorPicker colorPicker, TextField bSize) {
-
         bSize.setOnAction(e -> size = Double.parseDouble(bSize.getText()));
         graphicsContext.getCanvas().setOnMousePressed(mouseEvent -> {
             x = mouseEvent.getX();
             y = mouseEvent.getY();
             if (mouseEvent.isSecondaryButtonDown()) esRelleno = true;
-
         });
+
 
         graphicsContext.getCanvas().setOnMouseReleased(mouseEvent -> {
             x2 = mouseEvent.getX();
@@ -34,19 +32,19 @@ public class DrawRectangle extends DrawShape {
             double startX = Math.min(x, x2);
             double startY = Math.min(y, y2);
 
-            graphicsContext.setStroke(colorPicker.getValue());
             graphicsContext.setLineWidth(Double.parseDouble(bSize.getText()));
-
-
+            graphicsContext.setStroke(colorPicker.getValue());
             if (esRelleno) {
                 graphicsContext.setFill(colorPicker.getValue());
-                graphicsContext.fillRect(startX, startY, width, height);
+                graphicsContext.fillOval(startX, startY, width, height);
+
                 esRelleno = false;
             } else {
-                graphicsContext.setFill(Color.WHITE);
-                graphicsContext.strokeRect(startX, startY, width, height);
+                graphicsContext.strokeOval(startX, startY, width, height);
             }
+
         });
+
     }
 
     @Override
--- src/main/java/com/example/paint1_0/DrawPerfectCircle.java
+++ src/main/java/com/example/paint1_0/DrawPerfectCircle.java
@@ -3,50 +3,46 @@ package com.example.paint1_0;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.TextField;
-import javafx.scene.paint.Color;
 
-public class DrawRectangle extends DrawShape {
+public class DrawPerfectCircle extends DrawShape {
+    private boolean esRelleno = false;
     protected Double x2 = 0D;
     protected Double y2 = 0D;
-    boolean esRelleno = false;
 
-    public DrawRectangle(GraphicsContext graphicsContext) {
+    public DrawPerfectCircle(GraphicsContext graphicsContext) {
         super(graphicsContext);
     }
 
     @Override
     public void draw(ColorPicker colorPicker, TextField bSize) {
-
         bSize.setOnAction(e -> size = Double.parseDouble(bSize.getText()));
         graphicsContext.getCanvas().setOnMousePressed(mouseEvent -> {
             x = mouseEvent.getX();
             y = mouseEvent.getY();
             if (mouseEvent.isSecondaryButtonDown()) esRelleno = true;
-
         });
+
 
         graphicsContext.getCanvas().setOnMouseReleased(mouseEvent -> {
             x2 = mouseEvent.getX();
             y2 = mouseEvent.getY();
 
             double width = Math.abs(x - x2);
-            double height = Math.abs(y - y2);
             double startX = Math.min(x, x2);
             double startY = Math.min(y, y2);
-
-            graphicsContext.setStroke(colorPicker.getValue());
             graphicsContext.setLineWidth(Double.parseDouble(bSize.getText()));
-
-
+            graphicsContext.setStroke(colorPicker.getValue());
             if (esRelleno) {
                 graphicsContext.setFill(colorPicker.getValue());
-                graphicsContext.fillRect(startX, startY, width, height);
+                graphicsContext.fillOval(startX, startY, width, width);
+
                 esRelleno = false;
             } else {
-                graphicsContext.setFill(Color.WHITE);
-                graphicsContext.strokeRect(startX, startY, width, height);
+                graphicsContext.strokeOval(startX, startY, width, width);
             }
+
         });
+
     }
 
     @Override
